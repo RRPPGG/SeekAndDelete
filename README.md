0b686f23855483726c684c78bc66f34a9790eae4
Rafal Gajewski <rpga@op.pl>
2020-03-28 20:56:08 +0100

    Adding command line options

--- README.md
+++ README.md
@@ -1,15 +1,33 @@
 # SeekAndDelete
 SeekAndDelete was written as a simple to use tool for cleaning your computer.
+
 This is why it is a PowerShell script, not a C++/C#/Python program.
+
 There is no need to build or install anything, just download the file and use.
 
 ## Usage
 Place SeekAndDelete.ps1 file into a directory where you want to look for duplicated files.
+
 Be carefull, more files means longer execution time.
+
+## Advanced usage
+Running from command line allows you to use several additional options:
+
+-rootdir "path"         - run analyzys in given directory instead of script directory
+
+-filetype ".extension"  - analyze only files with given extension
+
+-ignoreName             - ignore names
+
+-ignoreSize             - ignore sizes
+
+-ignoreDate             - ignore modification dates
+
+Notice that you can't ignore name, size and date at the same time.
 
 ## Plans
 Usage instructions for PowerShell scripts
-Command line options for advanced users (root dir, compare options)
+
 Find duplicated directories
 
 ## Contact
--- SeekAndDelete.ps1
+++ SeekAndDelete.ps1
@@ -1,15 +1,25 @@
+param (
+    [string]$rootdir = $pwd,
+    [string]$filetype = "", #Empty means all
+    [switch]$ignoreName = $false,
+    [switch]$ignoreSize = $false,
+    [switch]$ignoreDate = $false
+ )
+
+if (($ignoreName -eq $true) -and ($ignoreSize -eq $true) -and($ignoreDate -eq $true))
+{
+    echo "Ignoring name, size and date makes no sense - exiting..."
+    pause
+}
+
 $PrintAllFiles = 0
-$RootPath = pwd
-$CompareByName = 1
-$CompareBySize = 1
-$CompareByDate = 1
-$FileExtension = "" #Empty means all
-$OutputFile = "$RootPath\DuplicatedFiles.txt"
+$ScriptPath = pwd
+$OutputFile = "$ScriptPath\DuplicatedFiles.txt"
 
 $AllFiles = New-Object System.Collections.ArrayList
 
 $ToDoDirs = New-Object System.Collections.ArrayList
-[void]$ToDoDirs.Add($RootPath)
+[void]$ToDoDirs.Add($rootdir)
 
 echo "SeekAndDelete starting, looking for files..."
 
@@ -26,7 +36,7 @@ while ($ToDoDirs.Count -gt 0)
         }
         else
         {
-            if (($FileExtension -eq "") -or ($FileExtension -eq [IO.Path]::GetExtension($File)))
+            if (($filetype -eq "") -or ($filetype -eq [IO.Path]::GetExtension($File)))
             {
                 $fileObject = New-Object -TypeName PSObject
                 $fileObject | Add-Member -Name 'Name' Noteproperty -Value $File.Name
@@ -60,21 +70,21 @@ for ($i=0; $i -lt $AllFiles.Count; $i++)
     $SameGroup += $AllFiles[$i].FullName
     for ($j=$i+1; $j -lt $AllFiles.Count; $j++)
     {
-        if ($CompareByName -eq 1)
+        if ($ignoreName -eq $false)
         {
             if ($AllFiles[$i].Name -ne $AllFiles[$j].Name)
             {
                 continue
             }
         }
-        if ($CompareBySize -eq 1)
+        if ($ignoreSize -eq $false)
         {
             if ($AllFiles[$i].Size -ne $AllFiles[$j].Size)
             {
                 continue
             }
         }
-        if ($CompareByDate -eq 1)
+        if ($ignoreDate -eq $false)
         {
             if ($AllFiles[$i].Date.ToString('u') -ne $AllFiles[$j].Date.ToString('u'))
             {
@@ -89,7 +99,7 @@ for ($i=0; $i -lt $AllFiles.Count; $i++)
         if ($AtLeastOneFound -eq 0)
         {
             $AtLeastOneFound = 1
-            "SeekAndDelete raport for $RootPath" > $OutputFile
+            "SeekAndDelete raport for $rootdir" > $OutputFile
         }
         "`nFollowing files are duplicates:" >> $OutputFile
         foreach ($DuplicatedFile in $SameGroup)
@@ -108,5 +118,5 @@ else
     echo "Your duplicated file list is in $OutputFile"
 }
 echo "Done. Thank you for using SeekAndDelete"
-
+cd $ScriptPath
 pause
